3e32e44ab5ba4d3ec8888017a10f64b7e77f88b2
Alexandre LE PAPE <alex@alepape.net>
2022-12-21 10:23:18 +1100

    CORS hack

--- alex.php
+++ alex.php
@@ -1,6 +1,7 @@
 <?php
 //header('Content-Type: application/x-mpegURL');
 header('Content-Type: text/plain; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 
 error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 'On');
---
+++ local.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/x-mpegURL');
+//header('Content-Type: text/plain; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 'On');
+
+$file = $_GET['f'];
+
+$filepath = __DIR__ .'/'.$file;
+$content = file_get_contents($filepath);
+
+header('Content-Length: ', mb_strlen($content, '8bit'));
+
+echo $output;
+
+?>
